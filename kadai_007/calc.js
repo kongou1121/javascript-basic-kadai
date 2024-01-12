27a27429ca4da70fa20a0131380f6e41cd6c7609
kongou1121 <kongou1121.2002@gmail.com>
2024-01-12 10:21:31 +0900

    textを追加

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,22 +1,19 @@
 // 変数numに1～12までのランダムな整数を代入する
-let num = Math.floor(Math.random() * 12) + 1;
+let num = Math.floor(Math.random() * 20) + 1;
 
+// 変数(num)が3と5の倍数の場合「3と5の倍数です」と出力する
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log('3と5の倍数です')
+}
 
 // 変数numが3の倍数の場合、「3の倍数です」と出力する
-if  (num % 3 === 0) {
+else if  (num % 3 === 0) {
   console.log('3の倍数です');
 } 
 // 変数numが5の倍数の場合、「5の倍数です」と出力する
 else if (num % 5 === 0) {
   console.log('5の倍数です');
 } 
-
-num = 15;
-
-// 変数(num)が3と5の倍数の場合「3と5の倍数です」と出力する
-if (num % 3 === 0 && num % 5 === 0) {
-  console.log('3と5の倍数です')
-}
 
 // それ以外の場合、 変数(num)を出力する
 else {
